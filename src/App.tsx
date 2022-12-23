17f8867ad42410a0e3ed9c804d954fb5c48c0034
norrico31 <norricobiason31@gmail.com>
2022-12-23 23:09:39 +0800

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Container } from 'react-bootstrap'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import NewNote from './pages/NewNote'
+
+function App() {
+	return (
+		<Container className='my-4'>
+			<Routes>
+				<Route path='/' element={<h1>Hello</h1>} />
+				<Route path='/new' element={<NewNote />} />
+				<Route path='/:id'>
+					<Route index element={<h1>Show</h1>} />
+					<Route path='edit' element={<h1>Edit</h1>} />
+				</Route>
+				<Route path='*' element={<Navigate to='/' />} />
+			</Routes>
+		</Container>
+	)
+}
+
+export default App
---
+++ src/pages/NewNote.tsx
@@ -0,0 +1,7 @@
+export default function NewNote() {
+    return (
+        <>
+            New Note
+        </>
+    )
+}
